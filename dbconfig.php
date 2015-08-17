2f2eb3a77d04e6313b2f2a9fe46d46375e69394d
FranzGithub <franzapps@gmail.com>
2015-08-17 14:15:06 +0200

    Del dbconfig.php

--- dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_SERVER', 'mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/');
-define('DB_USERNAME', 'adminxxb8b4L');    // DB username
-define('DB_PASSWORD', 'mw7h4dXVzl4I');    // DB password
-define('DB_DATABASE', 'sittning');      // DB name
-$connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die( "Unable to connect");
-$database = mysql_select_db(DB_DATABASE) or die( "Unable to select database");
-?>
