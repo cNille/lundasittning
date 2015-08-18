f73fd582c9766fc7c792156c3703cb3cb81b377e
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2015-08-18 14:05:22 +0200

    Remove echos

--- db/dbAjax.php
+++ db/dbAjax.php
@@ -1,10 +1,7 @@
 <?php
-	echo '1';
 	require_once 'dbHandler.php';
 	
-	echo '2';
 	if(isset($_POST['action']) && !empty($_POST['action'])) {
-		echo '3';
 		$dbHandler = new DatabaseHandler();
 		$action = $_POST['action'];
 	    
@@ -12,10 +9,8 @@
 	        case 'addSitting' : addSitting($dbHandler);break;
 	        case 'removeSitting' : removeSitting($dbHandler);break;
 	    }
-
 	    $dbHandler.disconnect();
 	}
-
 
 	function addSitting($dbHandler){
 		if(isset($_POST['date']) && !empty($_POST['date'])) {
@@ -24,9 +19,7 @@
 	}
 	
 	function removeSitting($dbHandler){
-		echo '4';
 		if(isset($_POST['date']) && !empty($_POST['date'])) {
-			echo '5';
 			$dbHandler->deleteSitting($_POST['date']);
 		}
 	}
