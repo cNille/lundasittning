8f4dd5642a22f8c0cb09da81f400efba27139542
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2015-08-18 17:09:20 +0200

    Fix fb-login-functionality

--- facebook-login/logout.php
+++ facebook-login/logout.php
@@ -4,5 +4,5 @@ session_unset();
     $_SESSION['FBID'] = NULL;
     $_SESSION['FULLNAME'] = NULL;
     $_SESSION['EMAIL'] =  NULL;
-header("Location: index.php");        // you can enter home page here ( Eg : header("Location: " ."http://www.krizna.com"); 
+header("Location: ../index.php"); 
 ?>
