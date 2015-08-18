63ca12818ddee4b900b5df1353533216d8b780f3
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2015-08-18 15:23:01 +0200

    Create sidenav

--- footer.php
+++ footer.php
@@ -1,4 +1,16 @@
 	
+
+
+	<div class="sidenav">
+		<h1>Menu</h1>
+		<ul>
+			<li>Boka sittning</li>
+			<li>Mina sittningar</li>
+			<li>Mitt konto</li>
+			<li>F.A.Q</li>
+			<li>Logga ut</li>
+		</ul>
+	</div>
 	</body>
 	<script src="js/removeEvent.js"></script>
 </html>
