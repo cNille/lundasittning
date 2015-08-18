bfda55404af6a0f8919414c6741f741c4d10a9b7
FranzGithub <franzapps@gmail.com>
2015-08-18 12:13:48 +0200

    Fix fetch eventgrid

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <?php 
 	include 'header.php';
  	require_once 'dbconfig.php'; 
- 	require_once 'database.php'; 
-
+ 	require_once 'database.php';
  	$db = new Database(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 	$db->openConnection();
-	
+
 	if(!$db->isConnected()) {
 		header("Location: cannotConnect.php");
 		exit();
 	}
 			
-	$exists = $db->userExists();
+	$sittings = $db->getSittings();
 	
 	$db->closeConnection();
-
 
  ?>
 
@@ -24,31 +22,22 @@
 	</div>
 	<div class="event-grid">
 		<?php 
-			if($exists){
-				echo 'yes'; 
-			} else {
-				echo 'no';
-			}
-	
-			/*
-			$result = mysql_query("SELECT * FROM sitting ORDER BY sittDate");
-	
-			while($row = mysql_fetch_assoc($result)) {
-			$date = $row["sittDate"];
-			?>
-			<div class="event-window" id="<?php echo $date; ?>">
-				<div class="event-window-date">
-					<?php echo $date; ?>
+			foreach($sittings as $row => $s) {
+				$date = date('j/n', strtotime($s[0]));
+				?>
+				<div class="event-window" id="<?php echo $date; ?>">
+					<div class="event-window-date">
+						<?php echo $date; ?>
+					</div>
+					<div class="event-window-spots">
+						Antal platser: 200
+					</div>
+					<div class="event-window-button">
+						<a href="#"> Se mer </a>
+					</div>
+					<button class="event-remove-button">Remove</button>
 				</div>
-				<div class="event-window-spots">
-					Antal platser: 200
-				</div>
-				<div class="event-window-button">
-					<a href="#"> Se mer </a>
-				</div>
-				<button class="event-remove-button">Remove</button>
-			</div>
-		<?php }*/ ?>
+		<?php } ?>
 	</div>
 </div>
 
