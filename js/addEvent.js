7894b49e6437f04324ebb9243b7eafd0b75d3b43
FranzGithub <franzapps@gmail.com>
2015-08-18 16:12:48 +0200

    Add create event

--- js/addEvent.js
+++ js/addEvent.js
@@ -1,4 +1,13 @@
-$(".event-add-button").click(function(){
+$("#event-creator-initiate").click(function(){
+	document.getElementById("event-creator-initiate").style.display = "none";
+	$("#event-creator").append( "<input type='text' name='date' id='newDate' placeholder='DD/MM' maxlength='5' autocomplete='off' >");
+	document.getElementById("newDate").focus();
+	
+	$("#event-creator").append( "<button id='reject'>X</button>");
+	$("#event-creator").append( "<button id='confirm'>Y</button>");
+});
+
+$("#reject").click(function(){
 	var temp = addEvent(this, "event-window");
 
 	$.ajax({
@@ -10,9 +19,27 @@ $(".event-add-button").click(function(){
 		}
 	});
 });
+
+$("#confirm").click(function(){
+	var temp = addEvent(this, "event-window");
+
+	$.ajax({
+		type: 'POST',
+		url: 'db/dbAjax.php',
+		data: 'action=addSitting&date=' + temp,
+		success: function(data){
+			// If you want, alert whatever your PHP script outputs
+		}
+	});
+});
+
+
             	
 function addEvent (el, cls) {
     while ((el = el.parentElement) && !el.classList.contains(cls));
     el.style.display = "none";
     return el.id;
+}
+
+function stopCreation (){
 }
