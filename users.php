2ed24ef14fcb70a454f82fba3b5d41f632f5f16a
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2015-10-11 18:01:39 +0200

    Add users-list page

---
+++ users.php
@@ -0,0 +1,52 @@
+<?php 
+	require_once 'header.php';
+ 	$dbHandler = new DatabaseHandler();
+
+ 	$access = $dbHandler->getAccessLevel($fbid, $restaurant->name);
+ 	requireAccessLevel(5, $access);
+ 	$users = $dbHandler->getUsers($restaurant->name);
+ 	$userTypes = $dbHandler->getUserTypes($access);
+	$dbHandler->disconnect();
+ ?>
+<div class="content">
+	<?php echo "<h2>Användare vid $restaurant->name</h2>"; ?>
+	<div class="section">
+		<h3></h3>
+		
+		<form action="scripts.php" method="POST">
+		<table>
+			<tr>
+				<th>Namn</th>
+				<th>Email</th>
+				<th>Phone</th>
+				<th></th>
+			</tr>
+			<?php
+				foreach ($users as $key => $u) {
+					$type = $u[5] != "" ? $u[5] : "Användare"
+					?>
+						<tr>
+							<?php echo "<td>$u[1]</td>"; ?>
+							<?php echo "<td>$u[2]</td>"; ?>
+							<?php echo "<td>$u[3]</td>"; ?>
+							<?php echo "<td>$u[5]</td>"; ?>
+							<?php echo "<td><input type='checkbox' class='typeBox' value='$u[0]' name='user[]' /></td>"; ?>
+						</tr>
+					<?php
+				}
+			?>
+		</table>
+		<input type="submit" name="updateUserType" value="Uppdatera markerade användare till" />
+		<select name="userType">
+			<?php
+				foreach ($userTypes as $key => $ut) {
+					echo "<option value='$ut[0]'>$ut[0]</option>";
+				}
+			?>
+		</select>
+	</form>
+	</div>
+</div>
+
+
+<?php include 'footer.php'; ?>
