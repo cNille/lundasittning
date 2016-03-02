c7b14c4569422b6dc7fc8fdc5178c064d06f1d2b
FranzGithub <franzapps@gmail.com>
2016-03-02 15:53:08 +0100

    Add Google Analytics

--- restaurant.php
+++ restaurant.php
@@ -1,5 +1,6 @@
 <?php 
     require_once 'header.php';
+    include_once("analyticstracking.php");
 
  	$dbHandler = new DatabaseHandler();
     $sittings = $dbHandler->getSittings(1, $restaurant[0]);
